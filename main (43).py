a71ec51ed3466131489592a883ff2674a7fa9376
Gazzzq <124045691+Gazzzq@users.noreply.github.com>
2023-06-06 13:53:23 +0300

    на 06.06.2023

--- main (43).py
+++ main (43).py
@@ -1,13 +1,8 @@
-fib = 0
-fib1 = 1
-print(fib1)
-print(fib2)
-n = 10
-i = 0
-while i<n:
-    sum = fib1 + fib2
-    print(sum)
-    fib1 = fib2
-    fib2 = sum
-    i = i+1
-  
+def is_prime(x):
+    f=True
+    for i in range(2, int(x**0.5+1)):
+        if x%i ==0:
+            f=False
+    return(f)
+a=int(input('Ввести число: '))
+print(is_prime(a))
